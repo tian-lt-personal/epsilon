d8d553e10bf228dd6dc89d4fffebd743c324b520
TIAN LIAO <tian.lt@outlook.com>
2026-02-10 23:05:34 +0800

    impl: div_n (#3)

--- src/epsilon/z.hpp
+++ src/epsilon/z.hpp
@@ -3,6 +3,7 @@
 
 // std
 #include <algorithm>
+#include <bit>
 #include <cassert>
 #include <concepts>
 #include <limits>
@@ -15,8 +16,23 @@ namespace epx {
 
 namespace details {
 
+template <container C>
+constexpr z<C> zero() {
+  return z<C>{};
+}
+
+template <container C>
+constexpr z<C> one() {
+  return z<C>{.digits = {1}};
+}
+
 template <class MaxD, std::unsigned_integral T>
-constexpr T umul(T lhs, T rhs, T& o) {
+constexpr auto umul(T lhs, T rhs) {
+  struct result_t {
+    T p0;
+    T p1;
+  };
+
   if constexpr (sizeof(T) >= sizeof(MaxD)) {
     constexpr T s = sizeof(T) * 4u;
     constexpr T mask = std::numeric_limits<T>::max() >> s;
@@ -26,17 +42,57 @@ constexpr T umul(T lhs, T rhs, T& o) {
     T w = lh + (ll >> s);
     w += hl;
     if (w < hl) hh += mask + 1u;
-    o = hh + (w >> s);
-    return (w << s) + (ll & mask);
+    return result_t{.p0 = (w << s) + (ll & mask), .p1 = hh + (w >> s)};
   } else {
     constexpr unsigned s = sizeof(T) * 8u;
     MaxD l = lhs, r = rhs;
     auto prod = l * r;
-    o = static_cast<T>(prod >> s);
-    return static_cast<T>(prod);
+    return result_t{.p0 = static_cast<T>(prod), .p1 = static_cast<T>(prod >> s)};
   }
 }
 
+// u0 - LSD, u1 - MSD
+template <class D>
+constexpr auto div_2d(D u0, D u1, D v) {
+  using W = wide_digit_t<D>;
+  struct result_t {
+    W q;
+    D r;
+  };
+  assert(v != 0);
+  W u = (static_cast<W>(u1) << (sizeof(D) * CHAR_BIT)) | u0;
+  return result_t{.q = static_cast<W>(u / v), .r = static_cast<D>(u % v)};
+}
+
+template <class C>
+constexpr auto bit_shift(C& digits, int offset) {
+  using D = typename C::value_type;
+  assert((sizeof(D) * CHAR_BIT) > (size_t)std::abs(offset));
+  if (offset > 0) {
+    // left shift
+    const D mask = ((1u << offset) - 1) << (sizeof(D) * CHAR_BIT - offset);
+    D cy = 0;
+    for (auto& d : digits) {
+      D t = (d << offset) | cy;
+      cy = (d & mask) >> (sizeof(D) * CHAR_BIT - offset);
+      d = t;
+    }
+    return cy;
+  } else if (offset < 0) {
+    offset = std::abs(offset);
+    // right shift
+    const D mask = (1u << offset) - 1;
+    D cy = 0;
+    for (auto d = digits.rbegin(); d != digits.rend(); ++d) {
+      D t = (*d >> offset) | cy;
+      cy = (*d & mask) << (sizeof(D) * CHAR_BIT - offset);
+      *d = t;
+    }
+    return cy;
+  } else {
+    return (D)0;
+  }
+}
 }  // namespace details
 
 template <container C>
@@ -141,7 +197,7 @@ template <container C>
 constexpr z<C> mul_n(const z<C>& lhs, const z<C>& rhs) {
   using D = typename z<C>::digit_type;
   if (is_zero(lhs) || is_zero(rhs)) {
-    return z<C>{};
+    return details::zero<C>();
   }
 
   z<C> r;
@@ -152,17 +208,127 @@ constexpr z<C> mul_n(const z<C>& lhs, const z<C>& rhs) {
   for (size_t j = 0; j < std::ranges::size(b); ++j) {
     D cy = 0;
     for (size_t i = 0; i < std::ranges::size(a); ++i) {
-      D o;
-      auto prod = details::umul<default_digit_t>(a[i], b[j], o);
-      prod += cy;
-      cy = (prod < cy ? 1u : 0u) + o;
-      r.digits[i + j] += prod;
-      if (r.digits[i + j] < prod) ++cy;
+      auto [p0, p1] = details::umul<default_digit_t>(a[i], b[j]);
+      p0 += cy;
+      cy = (p0 < cy ? 1u : 0u) + p1;
+      r.digits[i + j] += p0;
+      if (r.digits[i + j] < p0) ++cy;
     }
     r.digits[j + std::ranges::size(a)] = cy;
   }
   normalize(r);
   return r;
+}
+
+template <container C>
+constexpr auto div_n(const z<C>& u, typename z<C>::digit_type v) {
+  using D = typename z<C>::digit_type;
+  static_assert(sizeof(D) <= sizeof(max_digit_t));
+
+  struct {
+    z<C> q;
+    D r;
+  } res{};
+
+  auto& w = res.q.digits;
+  auto n = std::ranges::size(u.digits);
+  w.resize(n);
+  for (auto i = 0uz; i < n; ++i) {
+    auto j = n - 1 - i;
+    auto [q, r] = details::div_2d(u.digits[j], res.r, v);
+    w[j] = static_cast<D>(q);
+    res.r = r;
+  }
+  normalize(res.q);
+  return res;
+}
+
+template <container C>
+constexpr auto div_n(z<C> lhs, z<C> rhs) {
+  struct result_t {
+    z<C> q;
+    z<C> r;
+  };
+
+  if (is_zero(rhs)) [[unlikely]] {
+    throw divide_by_zero_error{};
+  }
+
+  auto rel = cmp_n(lhs, rhs);
+  if (rel > 0) {
+    if (std::ranges::size(rhs.digits) > 1) {
+      using D = typename z<C>::digit_type;
+      using W = wide_digit_t<D>;
+      constexpr W b = W{1} << (sizeof(D) * CHAR_BIT);
+      constexpr D mask = D{b - 1};
+
+      auto& u = lhs.digits;
+      auto& v = rhs.digits;
+      auto n = std::ranges::size(v);
+      auto m = std::ranges::size(u) - n;
+
+      z<C> q;
+      q.digits.resize(m + 1);
+
+      // D1. [Normalize]
+      const auto s = std::countl_zero(v.back());
+      details::bit_shift(v, (int)s);
+      u.push_back(details::bit_shift(u, (int)s));  // this ensures u[m+n] exists.
+
+      // D2. [Initialize j]
+      for (auto l = 0uz; l <= m; ++l) {
+        auto j = m - l;
+
+        // D3. [Calculate qhat]
+        auto [qhat, rhat] = details::div_2d(u[j + n - 1], u[j + n], v[n - 1]);
+        while (qhat >= b || qhat * v[n - 2] > rhat * b + u[j + n - 2]) {
+          --qhat;
+          rhat += v[n - 1];
+          if (rhat < v[n - 1]) break;  // continue if rhat < b.
+        }
+
+        // D4. [Multiply and subtract]
+        using I = std::make_signed_t<W>;
+        I borrow = 0, diff;
+        for (auto i = 0uz; i < n; ++i) {  // u[j+n]u[j+n-1]...u[j], v[n-1]v[n-2]...v[0]
+          W p = qhat * v[i];
+          diff = W{u[i + j]} - borrow - static_cast<W>(p & mask);
+          u[i + j] = static_cast<D>(diff);
+          borrow = (p >> (sizeof(D) * CHAR_BIT)) - (diff >> (sizeof(D) * CHAR_BIT));
+        }
+        diff = I{u[j + n]} - borrow;
+        u[j + n] = static_cast<D>(diff);
+        q.digits[j] = static_cast<D>(qhat);
+
+        // D5. [Test remainder]
+        if (diff < 0) {
+          // D6. [Add back]
+          --q.digits[j];
+          I carry = 0;
+          for (auto i = 0uz; i < n; ++i) {
+            W sum = W{u[i + j]} + W{v[i]} + carry;
+            u[i + j] = static_cast<D>(sum);
+            carry = sum >> (sizeof(D) * CHAR_BIT);
+          }
+          u[j + n] = u[j + n] + static_cast<D>(carry);
+        }
+      }  // D7. [Loop on j]
+      // D8. [Unnormalize]
+      details::bit_shift(u, -s);
+      normalize(lhs);
+      return result_t{.q = std::move(normalize(q)), .r = std::move(lhs)};
+    } else {
+      assert(std::ranges::size(rhs.digits) == 1);
+      auto res = div_n(lhs, rhs.digits[0]);
+      z<C> r{.digits = {res.r}};
+      normalize(r);
+      return result_t{.q = std::move(res.q), .r = std::move(r)};
+    }
+  } else if (rel < 0) {
+    return result_t{.q = details::zero<C>(), .r = std::move(lhs)};
+  } else {
+    return result_t{.q = details::one<C>(), .r = details::zero<C>()};
+  }
 }
 
 template <container C>
--- src/ut/n_tests.cpp
+++ src/ut/n_tests.cpp
@@ -189,4 +189,106 @@ TEST(n_tests, mul_n) {
   }
 }
 
+TEST(n_tests, div_n) {
+  {
+    {
+      sz zero, one = {.digits = {1}};
+      auto res = epx::div_n(zero, one);
+      EXPECT_TRUE(epx::is_zero(res.q));
+      EXPECT_TRUE(epx::is_zero(res.r));
+      EXPECT_THROW(epx::div_n(one, zero), epx::divide_by_zero_error);
+    }
+    {
+      sz zero;
+      sz a = {.digits = {0, 1}};
+      sz b = {.digits = {2}};
+      auto res = epx::div_n(a, b);  // 256 / 2 = 128 ... 0
+      EXPECT_EQ(sz{.digits = {0x80}}, res.q);
+      EXPECT_EQ(zero, res.r);
+    }
+    {
+      sz zero;
+      sz a = {.digits = {0, 0, 1}};
+      sz b = {.digits = {0, 2}};
+      auto res = epx::div_n(a, b);  // 65536 / 512 = 128 ... 0
+      EXPECT_EQ(sz{.digits = {0x80}}, res.q);
+      EXPECT_EQ(zero, res.r);
+    }
+    {
+      sz a = {.digits = {5, 0, 0x10}};
+      sz b = {.digits = {0x3f, 3}};
+      auto res = epx::div_n(a, b);  // 1048581 / 831 = 1261 ... 690
+      sz expected_q{.digits = {0xed, 0x4}};
+      sz expected_r{.digits = {0xb2, 0x2}};
+      EXPECT_EQ(expected_q, res.q);
+      EXPECT_EQ(expected_r, res.r);
+    }
+    {
+      sz a = {.digits = {5, 0, 0, 1}};
+      sz b = {.digits = {0x3f, 3}};
+      auto res = epx::div_n(a, b);  // 16777221 / 831 = 20189 ... 162
+      sz expected_q{.digits = {0xdd, 0x4e}};
+      EXPECT_EQ(expected_q, res.q);
+      EXPECT_EQ(sz{.digits = {0xa2}}, res.r);
+    }
+    {
+      sz one = {.digits = {1}};
+      sz a = {.digits = {0x12, 0x34, 0x56, 0x78, 0x9a, 0xbc, 0xde, 0xff}};
+      sz b = {.digits = {0x11, 0x34, 0x56, 0x78, 0x9a, 0xbc, 0xde, 0xff}};
+      auto res = epx::div_n(a, b);  // 18437381296131290130 / 18437381296131290129 = 1 ... 1
+      EXPECT_EQ(one, res.q);
+      EXPECT_EQ(one, res.r);
+    }
+    {
+      sz zero, one = {.digits = {1}};
+      sz a = {.digits = {0xff, 0xde, 0xbc, 0x9a, 0x78, 0x56, 0x34, 0x12}};
+      sz b = {.digits = {0xff, 0xde, 0xbc, 0x9a, 0x78, 0x56, 0x34, 0x12}};
+      auto res = epx::div_n(a, b);  // 1311768467463790335 / 1311768467463790335= 1 ... 0
+      EXPECT_EQ(one, res.q);
+      EXPECT_EQ(zero, res.r);
+    }
+    {
+      sz one = {.digits = {1}};
+      sz a = {.digits = {0xff, 0xde, 0xbc, 0x9a, 0x78, 0x56, 0x34, 0x12}};
+      sz b = {.digits = {0xff, 0xde, 0xbc, 0x9a, 0x78, 0x56, 0x34, 0x11}};
+      auto res = epx::div_n(a, b);  // 1311768467463790335 / 1239710873425862399 = 1 ... 72057594037927936
+      sz expected_r{.digits = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 1}};
+      EXPECT_EQ(one, res.q);
+      EXPECT_EQ(expected_r, res.r);
+    }
+    {
+      sz a = {
+          .digits = {0xff, 0xde, 0xbc, 0x9a, 0x78, 0x56, 0x34, 0x12, 0xff, 0xde, 0xbc, 0x9a, 0x78, 0x56, 0x34, 0x12}};
+      sz b = {.digits = {0xff, 0xde, 0xbc, 0x9a, 0x78, 0x56, 0x34, 0x11}};
+      auto res = epx::div_n(a, b);  // 24197857203266735141494478776148221695 / 1239710873425862399 =
+                                    // 19518952137926717496 ... 1080293969106388791
+      sz expected_q{.digits = {0x38, 0x30, 0xf9, 0xb8, 0x8d, 0x3f, 0xe1, 0xe, 1}};
+      sz expected_r{.digits = {0x37, 0x47, 0x95, 0x29, 0xa4, 0xf9, 0xfd, 0xe}};
+      EXPECT_EQ(expected_q, res.q);
+      EXPECT_EQ(expected_r, res.r);
+    }
+    {
+      sz a = {
+          .digits = {0xff, 0xde, 0xbc, 0x9a, 0x78, 0x56, 0x34, 0x12, 0xff, 0xde, 0xbc, 0x9a, 0x78, 0x56, 0x34, 0x12}};
+      auto res = epx::div_n(a, sz{.digits = {0x25}});  // 24197857203266735141494478776148221695 / 37 =
+                                                       // 653996140628830679499850777733735721 ... 18
+      sz expected_q{
+          .digits = {0x29, 0xa5, 0x81, 0xe8, 0xcb, 0x85, 0x4d, 0xbb, 0x14, 0xac, 0xd4, 0xcc, 0x7f, 0xf4, 0x7d}};
+      sz expected_r{.digits = {0x12}};
+      EXPECT_EQ(expected_q, res.q);
+      EXPECT_EQ(expected_r, res.r);
+    }
+    {
+      // verify d5 & d6: test remainder & add back
+      sz a = {.digits = {0x58, 0x95, 0xca, 0x39, 0xa6}};
+      sz b = {.digits = {0xcf, 0x99, 0xb7}};
+      auto res = epx::div_n(a, b);  // 713934148952 / 12032463 = 5933 ... 12021773
+      sz expected_q{.digits = {0xc5, 0xe7}};
+      sz expected_r{.digits = {0xd, 0x70, 0xb7}};
+      EXPECT_EQ(expected_q, res.q);
+      EXPECT_EQ(expected_r, res.r);
+    }
+  }
+}
+
 }  // namespace epxut
