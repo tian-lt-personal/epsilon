67d63ea73538e237ac9464b354c2f8119570b936
tian <tian.lt@outlook.com>
2026-02-16 20:19:48 +0800

    to decimal string

--- src/epsilon/chars.hpp
+++ src/epsilon/chars.hpp
@@ -20,7 +20,7 @@ std::optional<z<C>> try_from_chars(std::string_view chars) {
   using D = typename z<C>::digit_type;
 
   if constexpr (B == 10) {
-    z<C> ten = {.digits = {10}};
+    const z<C> ten = {.digits = {10}};
     z<C> res;
     for (auto ch : chars) {
       if (!isdigit(static_cast<unsigned int>(ch))) {
@@ -33,6 +33,32 @@ std::optional<z<C>> try_from_chars(std::string_view chars) {
       }
       res = res * ten + tmp;
     }
+    return res;
+  } else {
+    static_assert("not implemented.");
+  }
+}
+
+template <container C, int B = 10>
+std::string to_string(z<C> num) {
+  using D = typename z<C>::digit_type;
+  if constexpr (B == 10) {
+    if (is_zero(num)) {
+      return "0";
+    }
+
+    std::string res;
+    const z<C> ten = {.digits = {10}};
+    while (!is_zero(num)) {
+      auto [q, r] = div_n(num, ten);
+      if (is_zero(r)) {
+        res.push_back('0');
+      } else {
+        res.push_back(static_cast<char>(r.digits[0]) + '0');
+      }
+      num = q;
+    }
+    std::ranges::reverse(res);
     return res;
   } else {
     static_assert("not implemented.");
